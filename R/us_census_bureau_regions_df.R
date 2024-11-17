5fd6a9005a73641563b19528cc8fc4e36adb0780
Mufflyt <tyler.muffly@dhha.org>
2024-11-16 19:52:12 -0700

    us_census_bureau_regions_df

---
+++ R/us_census_bureau_regions_df.R
@@ -0,0 +1,13 @@
+#' U.S. Census Bureau Regions and Divisions
+#'
+#' This dataset provides region and division information for U.S. states.
+#'
+#' @format A data frame with 51 rows and 4 variables:
+#' \describe{
+#'   \item{State}{The name of the U.S. state (e.g., "California").}
+#'   \item{State.Code}{The two-letter postal abbreviation for the state.}
+#'   \item{Region}{The region of the state (e.g., "West").}
+#'   \item{Division}{The division of the state (e.g., "Pacific").}
+#' }
+#' @source Derived from U.S. Census Bureau data.
+"us_census_bureau_regions_df"
